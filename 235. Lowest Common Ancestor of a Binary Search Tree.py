f3c31cca824a0057146186e3c5ad111f2f490886
ChienChenCCLee <ChienChenCCLee@fih-foxconn.com>
2024-09-12 17:12:48 +0800

    Complete 235 and add 1448

--- 235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -39,4 +39,13 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val <= root.val <= q.val or q.val <= root.val <= p.val:
+            return root
+        
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        else:
+            return self.lowestCommonAncestor(root.right,p ,q)
+        
         
