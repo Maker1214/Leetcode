b3fdb55cfbd8fd86e24333a2c2ce3286e31f178a
ChienChenCCLee <ChienChenCCLee@fih-foxconn.com>
2024-09-24 17:53:14 +0800

    Add 219 and 978

---
+++ 978. Longest Turbulent Subarray.py
@@ -0,0 +1,60 @@
+# Given an integer array arr, return the length of a maximum size turbulent subarray of arr.
+
+# A subarray is turbulent if the comparison sign flips between each adjacent pair of elements in the subarray.
+
+# More formally, a subarray [arr[i], arr[i + 1], ..., arr[j]] of arr is said to be turbulent if and only if:
+
+# For i <= k < j:
+# arr[k] > arr[k + 1] when k is odd, and
+# arr[k] < arr[k + 1] when k is even.
+# Or, for i <= k < j:
+# arr[k] > arr[k + 1] when k is even, and
+# arr[k] < arr[k + 1] when k is odd.
+ 
+
+# Example 1:
+
+# Input: arr = [9,4,2,10,7,8,8,1,9]
+# Output: 5
+# Explanation: arr[1] > arr[2] < arr[3] > arr[4] < arr[5]
+# Example 2:
+
+# Input: arr = [4,8,12,16]
+# Output: 2
+# Example 3:
+
+# Input: arr = [100]
+# Output: 1
+ 
+
+# Constraints:
+
+# 1 <= arr.length <= 4 * 10 ** 4
+# 0 <= arr[i] <= 10 ** 9
+
+class Solution:
+    def maxTurbulenceSize(self, arr: list[int]) -> int:
+        res, currres = 0, 0
+        pre = arr[0]
+
+        for i in arr:
+            currdiff = (pre - i)
+            if currdiff == 0:
+                currres = 1
+            else:
+                # 目前的diff與前一次的diff乘積為負數才符合題意，兩者皆為正或皆為負時乘積皆大於0
+                if currdiff * prediff >= 0:
+                    currres = 2
+                else:
+                    currres += 1            
+            pre = i
+            prediff = currdiff
+            res = max(res, currres)
+        
+        return res
+
+nums = [4,8,12,16]
+test = Solution()
+
+print(test.maxTurbulenceSize(nums))
+
